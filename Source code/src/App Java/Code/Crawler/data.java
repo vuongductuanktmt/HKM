02e87e772381d584dd680003c228d1a34f45521f
vuongductuanktmt <anhtuanpro312@gmail.com>
2017-04-08 15:36:36 +0700

    update,fix code

---
+++ Source code/src/App Java/Code/Crawler/data.java
@@ -0,0 +1,156 @@
+package Database.HKM;
+
+import java.sql.Date;
+
+public class data {
+	private String __WebsiteName__;
+	// Node
+	// Event
+	private String __TitleEventProducts__;
+	private String __LinkEventProducts__;
+	private String __LinkImageEventProduct__;
+	// Selling Products
+	private String __TitleSellingProducts__;
+	private String __LinkSellingProducts__;
+	private String __LinkImageSellingProduct__;
+	// Promotional Products
+	private String __TitlePromotionalProducts__;
+	private String __LinkPromotionalProducts__;
+	private String __LinkImagePromotionalProduct__;
+	// Category
+	private String __TitleCategory__;
+	private String __LinkCategory__;
+	// more
+	private Date __TimeLastUpdateRecord__;
+	private int __SumRecord__;
+
+public data(String __WebsiteName__, String __TitleEventProducts__, String __LinkEventProducts__, String __LinkImageEventProduct__, String __TitleSellingProducts__, String __LinkSellingProducts__, String __LinkImageSellingProduct__, String __TitlePromotionalProducts__, String __LinkPromotionalProducts__, String __LinkImagePromotionalProduct__, String __TitleCategory__, String __LinkCategory__, Date __TimeLastUpdateRecord__, int __SumRecord__) {
+this.__WebsiteName__ = __WebsiteName__;
+this.__TitleEventProducts__ = __TitleEventProducts__;
+this.__LinkEventProducts__ = __LinkEventProducts__;
+this.__LinkImageEventProduct__ = __LinkImageEventProduct__;
+this.__TitleSellingProducts__ = __TitleSellingProducts__;
+this.__LinkSellingProducts__ = __LinkSellingProducts__;
+this.__LinkImageSellingProduct__ = __LinkImageSellingProduct__;
+this.__TitlePromotionalProducts__ = __TitlePromotionalProducts__;
+this.__LinkPromotionalProducts__ = __LinkPromotionalProducts__;
+this.__LinkImagePromotionalProduct__ = __LinkImagePromotionalProduct__;
+this.__TitleCategory__ = __TitleCategory__;
+this.__LinkCategory__ = __LinkCategory__;
+this.__TimeLastUpdateRecord__ = __TimeLastUpdateRecord__;
+this.__SumRecord__ = __SumRecord__;
+}
+
+	public String getWebsiteName__() {
+		return __WebsiteName__;
+	}
+
+	public void setWebsiteName__(String __WebsiteName__) {
+		this.__WebsiteName__ = __WebsiteName__;
+	}
+
+	public String getTitleEventProducts__() {
+		return __TitleEventProducts__;
+	}
+
+	public void setTitleEventProducts__(String __TitleEventProducts__) {
+		this.__TitleEventProducts__ = __TitleEventProducts__;
+	}
+
+	public String getLinkEventProducts__() {
+		return __LinkEventProducts__;
+	}
+
+	public void setLinkEventProducts__(String __LinkEventProducts__) {
+		this.__LinkEventProducts__ = __LinkEventProducts__;
+	}
+
+	public String getLinkImageEventProduct__() {
+		return __LinkImageEventProduct__;
+	}
+
+	public void setLinkImageEventProduct__(String __LinkImageEventProduct__) {
+		this.__LinkImageEventProduct__ = __LinkImageEventProduct__;
+	}
+
+	public String getTitleSellingProducts__() {
+		return __TitleSellingProducts__;
+	}
+
+	public void setTitleSellingProducts__(String __TitleSellingProducts__) {
+		this.__TitleSellingProducts__ = __TitleSellingProducts__;
+	}
+
+	public String getLinkSellingProducts__() {
+		return __LinkSellingProducts__;
+	}
+
+	public void setLinkSellingProducts__(String __LinkSellingProducts__) {
+		this.__LinkSellingProducts__ = __LinkSellingProducts__;
+	}
+
+	public String getLinkImageSellingProduct__() {
+		return __LinkImageSellingProduct__;
+	}
+
+	public void setLinkImageSellingProduct__(String __LinkImageSellingProduct__) {
+		this.__LinkImageSellingProduct__ = __LinkImageSellingProduct__;
+	}
+
+	public String getTitlePromotionalProducts__() {
+		return __TitlePromotionalProducts__;
+	}
+
+	public void setTitlePromotionalProducts__(String __TitlePromotionalProducts__) {
+		this.__TitlePromotionalProducts__ = __TitlePromotionalProducts__;
+	}
+
+	public String getLinkPromotionalProducts__() {
+		return __LinkPromotionalProducts__;
+	}
+
+	public void setLinkPromotionalProducts__(String __LinkPromotionalProducts__) {
+		this.__LinkPromotionalProducts__ = __LinkPromotionalProducts__;
+	}
+
+	public String getLinkImagePromotionalProduct__() {
+		return __LinkImagePromotionalProduct__;
+	}
+
+	public void setLinkImagePromotionalProduct__(String __LinkImagePromotionalProduct__) {
+		this.__LinkImagePromotionalProduct__ = __LinkImagePromotionalProduct__;
+	}
+
+	public String getTitleCategory__() {
+		return __TitleCategory__;
+	}
+
+	public void setTitleCategory__(String __TitleCategory__) {
+		this.__TitleCategory__ = __TitleCategory__;
+	}
+
+	public String getLinkCategory__() {
+		return __LinkCategory__;
+	}
+
+	public void setLinkCategory__(String __LinkCategory__) {
+		this.__LinkCategory__ = __LinkCategory__;
+	}
+
+	public Date getTimeLastUpdateRecord__() {
+		return __TimeLastUpdateRecord__;
+	}
+
+	public void setTimeLastUpdateRecord__(Date __TimeLastUpdateRecord__) {
+		this.__TimeLastUpdateRecord__ = __TimeLastUpdateRecord__;
+	}
+
+	public int getSumRecord__() {
+		return __SumRecord__;
+	}
+
+	public void setSumRecord__(int __SumRecord__) {
+		this.__SumRecord__ = __SumRecord__;
+	}
+
+}
