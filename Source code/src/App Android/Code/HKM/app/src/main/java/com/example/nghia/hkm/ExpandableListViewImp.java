60cf08d2384456c54a77b170c64eef0270da0efc
HuuTran <huu2107@gmail.com>
2017-04-27 00:09:26 +0700

    check again

--- Source code/src/App Android/Code/HKM/app/src/main/java/com/example/nghia/hkm/ExpandableListViewImp.java
+++
@@ -1,9 +0,0 @@
-package com.example.nghia.hkm;
-
-
-public interface ExpandableListViewImp {
-    void onRefresh();
-
-    void onButtonExpandableClick(int position);
-//    void onCommentsClick(int position);
-    }
--- Source code/src/App Android/Code/HKM/app/src/main/java/com/example/nghia/hkm/Interface/OnLoadMoreListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.nghia.hkm.Interface;
-
-/**
- * Created by huu21 on 26/04/2017.
- */
-
-public interface OnLoadMoreListener {
-    void onLoadMore();
-}
