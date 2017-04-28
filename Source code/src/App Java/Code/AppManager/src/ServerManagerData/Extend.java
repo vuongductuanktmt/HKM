9748e0f88d6e6f79a95dbfb51587f1b01fbe25bf
Vuong Duc Tuan <tuan.vd@cloudjetkpi.com>
2017-04-28 10:07:53 +0700

    change folder

--- Source code/src/App Java/Code/AppManager/src/ServerManagerData/Extend.java
+++
@@ -1,5 +0,0 @@
-package ServerManagerData;
-
-public class Extend {
-
-}
--- Source code/src/App Java/Code/AppManager/src/ServerManagerData/Security.java
+++
@@ -1,7 +0,0 @@
-package ServerManagerData;
-public class Security {
-	private String value[] = {"Ai Yeu Bac Ho Chi Minh","Bac Ho Cho Em Tat Ca","Ban Tay Me","Minh La Gi Cua Nhau"};
-	public String getValue(int id){
-		return this.value[id];
-	}
-}
